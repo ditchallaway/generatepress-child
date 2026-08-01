9e8e0b9743b026b098d50546db9af3ca104d0da3
jonnybot <ditchallaway@users.noreply.github.com>
2026-07-31 18:08:20 -0700

    feat: enqueue custom stylesheet and apply accessibility-focused contrast overrides for SureCart checkout components

--- functions.php
+++ functions.php
@@ -7,7 +7,18 @@ function enqueue_jquery() {
 
  add_action('wp_enqueue_scripts', 'enqueue_jquery');
 
-
+/**
+ * Enqueue child theme custom styles
+ */
+function brokertricks_enqueue_custom_styles() {
+    wp_enqueue_style(
+        'brokertricks-custom',
+        get_stylesheet_directory_uri() . '/css/custom.css',
+        array(),
+        filemtime( get_stylesheet_directory() . '/css/custom.css' )
+    );
+}
+add_action( 'wp_enqueue_scripts', 'brokertricks_enqueue_custom_styles' );
 
 /**
  * Require custom fulfillment dashboard script
